3b378ef33021b849b3661cb5e2e4d88643d558fa
Serhii <sysoev.khai@gmail.com>
2023-09-15 09:32:50 +0300

    added nav panel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,25 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Products from '../pages/Products';
+import NotFound from 'pages/NotFound';
+import { StyledLink } from './App.styled';
 
 function App() {
+  console.log('we are here');
   return (
     <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>Unfortunately nothing was found</div>;
+}
+
+export default NotFound;
