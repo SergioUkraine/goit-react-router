da054b2a4ad72fd46dbf075ee01a616b5e58d38c
Serhii <sysoev.khai@gmail.com>
2023-09-17 20:06:14 +0300

    componen Pokemons refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import * as API from '../services/api';
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 //pages
@@ -25,46 +22,7 @@ const Team = lazy(() => import('./Team'));
 const Reviews = lazy(() => import('./Reviews'));
 const SharedLayout = lazy(() => import('./SharedLayout'));
 
-const POKEMONS_PER_PAGE = 50;
-
 function App() {
-  const [pokemons, setPokemons] = useState([]);
-  const [page, setPage] = useState(1);
-  const [disableNextBtn, setDisableNextBtn] = useState(false);
-  const [disablePreviousBtn, setDisablePreviousBtn] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const pokemonSearchQuery = searchParams.get('pokemon')
-    ? searchParams.get('pokemon')
-    : '';
-  useEffect(() => {
-    async function getPokemons() {
-      try {
-        const offset = page * POKEMONS_PER_PAGE - POKEMONS_PER_PAGE;
-        const responce = await API.getPokemonList(offset, POKEMONS_PER_PAGE);
-        setPokemons(responce.results);
-        if (page === 1) setDisablePreviousBtn(true);
-        else setDisablePreviousBtn(false);
-        if (responce.count - (page - 1) * POKEMONS_PER_PAGE < POKEMONS_PER_PAGE)
-          setDisableNextBtn(true);
-        else setDisableNextBtn(false);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getPokemons();
-  }, [page]);
-
-  const pokemonSearch = async e => {
-    const pokemonSearch = e.currentTarget.value.toLowerCase();
-    setSearchParams(pokemonSearch ? { pokemon: pokemonSearch } : {});
-  };
-
-  const pokemonFilter = () => {
-    return pokemons.filter(pokemon =>
-      pokemon.name.toLowerCase().includes(pokemonSearchQuery)
-    );
-  };
-
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
@@ -74,25 +32,7 @@ function App() {
           <Route path="team" element={<Team />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route
-          path="/pokemon"
-          element={
-            <Pokemon
-              pokemons={pokemonFilter()}
-              onNext={() => {
-                setPage(s => s + 1);
-              }}
-              onPrevoious={() => {
-                setPage(s => s - 1);
-              }}
-              offset={page * POKEMONS_PER_PAGE - POKEMONS_PER_PAGE}
-              isNext={disableNextBtn}
-              isPrevious={disablePreviousBtn}
-              handleChangeInput={pokemonSearch}
-              searchValue={pokemonSearchQuery}
-            />
-          }
-        />
+        <Route path="/pokemon" element={<Pokemon />} />
         <Route path="/pokemon/:id" element={<PokemonDetails />} />
         <Route path="*" element={<NotFound />} />
       </Route>
--- src/pages/Pokemon/Pokemon.jsx
+++ src/pages/Pokemon/Pokemon.jsx
@@ -1,24 +1,80 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import * as API from '../../services/api';
+
 import PokemonList from 'components/PokemonList';
-function Pokemons({
-  pokemons,
-  onPrevoious,
-  onNext,
-  isPrevious,
-  isNext,
-  handleChangeInput,
-  searchValue,
-  ...otherProps
-}) {
+
+const POKEMONS_PER_PAGE = 50;
+
+function Pokemons() {
+  const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(1);
+  const [disableNextBtn, setDisableNextBtn] = useState(false);
+  const [disablePreviousBtn, setDisablePreviousBtn] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pokemonSearchQuery = searchParams.get('pokemon')
+    ? searchParams.get('pokemon')
+    : '';
+  useEffect(() => {
+    async function getPokemons() {
+      try {
+        const offset = page * POKEMONS_PER_PAGE - POKEMONS_PER_PAGE;
+        const responce = await API.getPokemonList(offset, POKEMONS_PER_PAGE);
+        setPokemons(responce.results);
+        if (page === 1) setDisablePreviousBtn(true);
+        else setDisablePreviousBtn(false);
+        if (responce.count - (page - 1) * POKEMONS_PER_PAGE < POKEMONS_PER_PAGE)
+          setDisableNextBtn(true);
+        else setDisableNextBtn(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getPokemons();
+  }, [page]);
+
+  const handleClickPrevious = () => {
+    setPage(s => s - 1);
+  };
+
+  const handleClickNext = () => {
+    setPage(s => s + 1);
+  };
+
+  const pokemonSearch = async e => {
+    const pokemonSearch = e.currentTarget.value.toLowerCase();
+    setSearchParams(pokemonSearch ? { pokemon: pokemonSearch } : {});
+  };
+
+  const pokemonFilter = () => {
+    return pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(pokemonSearchQuery)
+    );
+  };
+
   return (
     <div>
       <span>Pokemon: </span>
 
-      <input onChange={handleChangeInput} value={searchValue}></input>
-      <PokemonList pokemons={pokemons} {...otherProps} />
-      <button type="button" onClick={onPrevoious} disabled={isPrevious}>
+      <input onChange={pokemonSearch} value={pokemonSearchQuery}></input>
+      <PokemonList
+        pokemons={pokemonFilter()}
+        offset={page * POKEMONS_PER_PAGE - POKEMONS_PER_PAGE}
+      />
+      <button
+        name="previous"
+        type="button"
+        onClick={handleClickPrevious}
+        disabled={disablePreviousBtn}
+      >
         Previous
       </button>
-      <button type="button" onClick={onNext} disabled={isNext}>
+      <button
+        name="next"
+        type="button"
+        onClick={handleClickNext}
+        disabled={disableNextBtn}
+      >
         Next
       </button>
     </div>
